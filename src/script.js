b8028bf74ac8482c831b0e944dc2edb1865d1940
Ícaro Nascimento <ikaro_460@hotmail.com>
2022-02-02 11:10:35 -0300

    icaro commit inicial

--- src/script.js
+++ src/script.js
@@ -1,13 +1,37 @@
-function aboveAverageGrades() {}
+let grades = [9, 10, 8, 2, 1, 9, 0, 6, 7, 3, 8]
+console.log(grades);
 
-function averageGrades() {}
+function aboveAverageGrades(a) {
+    return a.filter(b => b > 8);
+}
+console.log(aboveAverageGrades(grades));
 
-function belowAverageGrades() {}
+function averageGrades(a) {
+    return a.filter(b => b === 8);
+}
+console.log(averageGrades(grades));
 
-function sumOfAllGrades() {}
+function belowAverageGrades(a) {
+    return a.filter(b => b < 8);
+}
+console.log(belowAverageGrades(grades));
 
-function averageOfGrades() {}
+function sumOfAllGrades(a) {
+    return a.reduce((b,c) => b + c);
+}
+console.log(sumOfAllGrades(grades));
 
-function maxGrade() {}
+function averageOfGrades(a) {
+    return Math.round(a.reduce((b,c) => b + c)/a.length * 10)/10;
+}
+console.log(averageOfGrades(grades));
 
-function minGrade() {}
+function maxGrade(a) {
+    return Math.max.apply(0, a);
+}
+console.log(maxGrade(grades));
+
+function minGrade(a) {
+    return Math.min.apply(0, a);
+}
+console.log(minGrade(grades));
